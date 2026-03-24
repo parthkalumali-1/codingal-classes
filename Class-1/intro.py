593a61e631a0a96a686668bf6f8f84d65d978cc2
parth <parthkalumali@gmail.com>
2026-03-24 19:33:26 +0530

    first commit

---
+++ Class-1/intro.py
@@ -0,0 +1,33 @@
+print ("hello") 
+print(1)
+print(12)
+print(1, 2)
+print('PARTH')
+print("I am Parth")
+print("qwertyuiop")
+print("I \nam \nlearning\n python")
+print("hi \n" * 5)
+
+print(1, 2, "parth", "I am parth")
+
+print(12 + 20)
+print("12 + 20=", 12 + 20)
+print(f"12 + 20 = {12 + 20}")
+print(f"12 - 20 = {12 - 20}")
+print(f"12 * 20 = {12 * 20}")
+print(f"12 / 20 = {12 / 20}")
+
+print(f"240 remainder 10 = {240 % 10}")  
+print(f"240 / 10 = {240 / 10}")
+print(f"240 // 10 = {240 // 10}") 
+
+print(f"20 ** 3 = {20 ** 3}")
+print(f"6 ** 10 = {6 ** 10}")
+print(25 ** 0.5)
+
+x = int(input("Enter a number: "))
+y = int(input("Enter a number: "))
+
+print(f"x value is {x}")
+print(f"y value is {y}")
+print(f"sum of {x} and {y} is {x * y}")
