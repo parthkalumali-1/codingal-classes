a834cee2cb0ca29344d874ec7c0b276173d6a5df
parth <parthkalumali@gmail.com>
2026-03-24 20:13:50 +0530

    Variable and Variable types

---
+++ class 2/intro.py
@@ -0,0 +1,11 @@
+x1 = 17
+y2 = 21 
+z = "parth"  
+isFromIndia_2 = True  
+
+print(f"value of x1 is {x1}")
+print(f"type of data stored in x1 is {type(x1)}")
+print(f"value of y2 is {y2}")
+print(f"type of data stored in y2 is {type(y2)}")
+print(f"value of y2 is {z}")
+print(f"type of data stored in z is {z}")
