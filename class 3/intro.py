df7c5577db1baf9dc740a8f9cd9d1880dc39eadb
parth <parthkalumali@gmail.com>
2026-04-28 20:03:14 +0530

    class 3

---
+++ class 3/intro.py
@@ -0,0 +1,11 @@
+age = int(input("Enter your age: "))
+if age < 18:
+    print("You are not adult")
+else:
+    print("You are adult")
+
+num = int(input("Enter number: "))
+if num % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
---
+++ class 3/intro1.py
@@ -0,0 +1,6 @@
+num = int(input("Enter Number: "))
+
+if num % 3 == 0 and num % 5 == 0:
+    print("Divisible by 3 and 5 both")
+else:
+    print("Not Divisible by both")
